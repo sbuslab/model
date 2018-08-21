e61ada0fd4d90a908dcca1b3f8adfc696e7fc98f
Vladimir Kuptsov <vkuptcov@gmail.com>
2018-08-21 20:47:59 +0200

    + FacadeIgnore annotation support

---
+++ src/main/java/com/sbuslab/model/FacadeIgnore.java
@@ -0,0 +1,13 @@
+package com.sbuslab.model;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FacadeIgnore {
+
+    boolean value() default true;
+}
