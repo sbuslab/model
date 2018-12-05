891c87d6e6724e43630dd2cfceb591dd8540ce31
kulikov <kulikov.dm@gmail.com>
2018-12-05 15:10:00 +0300

    add optional `_embedded` to all sbus errors

--- src/main/java/com/sbuslab/model/ErrorResponseBody.java
+++ src/main/java/com/sbuslab/model/ErrorResponseBody.java
@@ -1,9 +1,9 @@
 package com.sbuslab.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.Map;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 public class ErrorResponseBody {
@@ -15,11 +15,15 @@ public class ErrorResponseBody {
     @JsonProperty("_links")
     private final Map<String, Object> links;
 
-    @java.beans.ConstructorProperties({"message", "error", "links"})
-    public ErrorResponseBody(String message, String error, Map<String, Object> links) {
+    @JsonProperty("_embedded")
+    private final Map<String, Object> embedded;
+
+    @java.beans.ConstructorProperties({"message", "error", "links", "embedded"})
+    public ErrorResponseBody(String message, String error, Map<String, Object> links, Map<String, Object> embedded) {
         this.message = message;
         this.error = error;
         this.links = links;
+        this.embedded = embedded;
     }
 
     public String getMessage() {
@@ -34,6 +38,10 @@ public class ErrorResponseBody {
         return this.links;
     }
 
+    public Map<String, Object> getEmbedded() {
+        return this.embedded;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,17 +49,19 @@ public class ErrorResponseBody {
         ErrorResponseBody that = (ErrorResponseBody) o;
         return Objects.equals(message, that.message) &&
                 Objects.equals(error, that.error) &&
-                Objects.equals(links, that.links);
+                Objects.equals(links, that.links) &&
+                Objects.equals(embedded, that.embedded);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(message, error, links);
+        return Objects.hash(message, error, links, embedded);
     }
 
     public String toString() {
         return "ErrorResponseBody(message=" + this.getMessage() +
                 ", error=" + this.getError() +
-                ", links=" + this.getLinks() + ")";
+                ", links=" + this.getLinks() +
+                ", embedded=" + this.getLinks() + ")";
     }
 }
--- src/main/scala/com/sbuslab/model/ErrorMessage.scala
+++ src/main/scala/com/sbuslab/model/ErrorMessage.scala
@@ -8,23 +8,24 @@ class ErrorMessage(
   msg: String,
   cause: Throwable = null,
   val error: String = null,
-  val _links: java.util.Map[String, Object] = null
+  val _links: java.util.Map[String, Object] = null,
+  val _embedded: java.util.Map[String, Object] = null
 ) extends RuntimeException(msg, cause)
 
 
 object ErrorMessage {
-  def fromCode(code: Int, msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null): ErrorMessage = code match {
-    case 400 ⇒ new BadRequestError(msg, cause, error)
-    case 401 ⇒ new UnauthorizedError(msg, cause, error)
-    case 403 ⇒ new ForbiddenError(msg, cause, error, _links)
-    case 404 ⇒ new NotFoundError(msg, cause, error)
-    case 405 ⇒ new MethodNotAllowedError(msg, cause, error)
-    case 409 ⇒ new ConflictError(msg, cause, error)
-    case 429 ⇒ new TooManyRequestError(msg, cause, error)
-    case 456 ⇒ new UnrecoverableError(msg, cause, error)
-    case 500 ⇒ new InternalServerError(msg, cause, error)
-    case 503 ⇒ new ServiceUnavailableError(msg, cause, error)
-    case _   ⇒ new ErrorMessage(code, msg, cause, error, _links)
+  def fromCode(code: Int, msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null): ErrorMessage = code match {
+    case 400 ⇒ new BadRequestError(msg, cause, error, _links, _embedded)
+    case 401 ⇒ new UnauthorizedError(msg, cause, error, None, _links, _embedded)
+    case 403 ⇒ new ForbiddenError(msg, cause, error, _links, _embedded)
+    case 404 ⇒ new NotFoundError(msg, cause, error, _links, _embedded)
+    case 405 ⇒ new MethodNotAllowedError(msg, cause, error, _links, _embedded)
+    case 409 ⇒ new ConflictError(msg, cause, error, _links, _embedded)
+    case 429 ⇒ new TooManyRequestError(msg, cause, error, _links, _embedded)
+    case 456 ⇒ new UnrecoverableError(msg, cause, error, _links, _embedded)
+    case 500 ⇒ new InternalServerError(msg, cause, error, _links, _embedded)
+    case 503 ⇒ new ServiceUnavailableError(msg, cause, error, _links, _embedded)
+    case _   ⇒ new ErrorMessage(code, msg, cause, error, _links, _embedded)
   }
 }
 
@@ -39,54 +40,64 @@ object UnrecoverableFailures {
 }
 
 
-class BadRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(400, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class BadRequestError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(400, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class UnauthorizedError(msg: String, cause: Throwable = null, error: String = null, val schema: Option[String] = None) extends ErrorMessage(401, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class UnauthorizedError(msg: String, cause: Throwable = null, error: String = null, val schema: Option[String] = None, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(401, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, None, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, None, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, None, null, null)
 }
 
-class ForbiddenError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null) extends ErrorMessage(403, msg, cause, error, _links) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class ForbiddenError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(403, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class NotFoundError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(404, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class NotFoundError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(404, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class MethodNotAllowedError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(405, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class MethodNotAllowedError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(405, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class ConflictError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(409, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class ConflictError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(409, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class TooManyRequestError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(429, msg, cause, error) {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class TooManyRequestError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(429, msg, cause, error, _links, _embedded) {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class InternalServerError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(500, msg, cause, error) {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class InternalServerError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(500, msg, cause, error, _links, _embedded) {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class ServiceUnavailableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(503, msg, cause, error) {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class ServiceUnavailableError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(503, msg, cause, error, _links, _embedded) {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
-class UnrecoverableError(msg: String, cause: Throwable = null, error: String = null) extends ErrorMessage(456, msg, cause, error) with UnrecoverableFailure {
-  def this(msg: String) = this(msg, null, null)
-  def this(msg: String, cause: Throwable) = this(msg, cause, null)
+class UnrecoverableError(msg: String, cause: Throwable = null, error: String = null, _links: java.util.Map[String, Object] = null, _embedded: java.util.Map[String, Object] = null) extends ErrorMessage(456, msg, cause, error, _links, _embedded) with UnrecoverableFailure {
+  def this(msg: String) = this(msg, null, null, null, null)
+  def this(msg: String, cause: Throwable) = this(msg, cause, null, null, null)
+  def this(msg: String, cause: Throwable, error: String) = this(msg, cause, error, null, null)
 }
 
 
