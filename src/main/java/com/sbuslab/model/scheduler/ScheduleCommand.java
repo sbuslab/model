1e5be2fae22b9e910ad7efa6fd4daace14593824
kulikov <kulikov.dm@gmail.com>
2019-03-15 23:04:04 +0300

    scheduler: add ScheduleCommand command

---
+++ src/main/java/com/sbuslab/model/scheduler/ScheduleCommand.java
@@ -0,0 +1,27 @@
+package com.sbuslab.model.scheduler;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleCommand {
+
+    private String scheduleId;
+
+    private Long atTime;
+
+    private Long period;
+
+    @NotNull
+    private String routingKey;
+
+    private Object body;
+}
