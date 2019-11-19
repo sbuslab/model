64a2850a602b09bbf20e93b0d9d034c8cf5f6c43
Andrey Pshenkin <a.pshenkin@rambler-co.ru>
2019-11-19 15:45:43 +0300

    add secure string model and serializer

---
+++ src/main/java/com/sbuslab/model/SecureStringSerializer.java
@@ -0,0 +1,28 @@
+package com.sbuslab.model;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+
+public class SecureStringSerializer extends StdSerializer<SecureString> {
+
+    public SecureStringSerializer() {
+        this(null);
+    }
+
+    public SecureStringSerializer(Class<SecureString> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(
+        SecureString value, JsonGenerator jgen, SerializerProvider provider)
+      throws IOException, JsonProcessingException {
+
+        jgen.writeString(value.asString());
+    }
+}
